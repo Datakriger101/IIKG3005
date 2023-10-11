00e0b6874a23acdb690f984da46bd8361d27f44d
Eskil Refsgaard <eskil.refsgaard@gmail.com>
2023-10-11 14:22:40 +0200

    dontrun

---
+++ 06module/backend/variables.tf
@@ -0,0 +1,29 @@
+variable "rg_backend_name" {
+  type        = string
+  description = "value"
+}
+
+variable "rg_backend_location" {
+  type        = string
+  description = "value"
+}
+
+variable "sa_backend_name" {
+  type        = string
+  description = "value"
+}
+
+variable "sc_backend_name" {
+  type        = string
+  description = "value"
+}
+
+variable "kv_backend_name" {
+  type = string
+  description = "value"
+}
+
+variable "sa_backend_accesskey_name" {
+  type = string
+  description = "value"
+}
---
+++ 06module/github-actions/providers.tf
@@ -0,0 +1,21 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.75.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "ez-tg-backend-tfstate"
+    storage_account_name = "sabetfs8w79oreppe"
+    container_name       = "tfstate"
+    key                  = "ga-test.terraform.tfstate"
+    # Bruke key for å definere applikasjons om brukes. Noe smart.
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
