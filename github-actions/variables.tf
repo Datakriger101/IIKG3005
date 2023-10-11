d6675f499662ae695c35fe0de05f47b35eff3bce
Eskil Refsgaard <eskil.refsgaard@gmail.com>
2023-10-11 14:34:43 +0200

    test

--- github-actions/variables.tf
+++ github-actions/variables.tf
@@ -12,7 +12,7 @@ variable "sa_name" {
 }
 variable "source_content" {
   type    = string
-  default = "<h1>Hello CI/CD, this page was made with Terraform 2.5</h1>"
+  default = "<h1>Hello CI/CD, this page was made with Terraform 2.5</h1><p>Update del 1</p>"
 }
 variable "index_document" {
   type    = string
