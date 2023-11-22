ed41b785539203751f790c761aa4acfc9d89f4d0
Eskil Refsgaard <eskil@refsgaard.no>
2023-11-22 12:58:46 +0100

    Added new shit

---
+++ Weekly-assignments/07Week_infra-testing/demo-infra-copy/providers.tf
@@ -0,0 +1,24 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.75.0"
+    }
+  }
+
+  // Her må verdiene endres slik at de passer
+  backend "azurerm" {
+    resource_group_name  = "ez-rg-mod05-backend"
+    storage_account_name = "ezsaacbackend92x1ikov"
+    container_name       = "tfstate"
+    key                  = "mod07-val/terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+}
+
+
